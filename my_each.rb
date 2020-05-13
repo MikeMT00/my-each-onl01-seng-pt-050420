806de517f00ca8466de7193113e574f64b71f2a8
Michael Thomas <michaelthomas0611@gmail.com>
2020-05-13 20:40:35 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,7 @@
-def my_each(collection)
-  i = 0
-  while i < collection.length
-    yield(collection[i])
+def my_each(collection)         # Assign the method and the variable
+  i = 0                         # element is equal to zero
+  while i < collection.length   # calls on while i is less than the variable length
+    yield(collection[i])        # yields the variable element
     i = i + 1
   end
   collection
